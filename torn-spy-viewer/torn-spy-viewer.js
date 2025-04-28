07bbc6eabe2a389bdb7efb8a5d87f8ab0ea52ed4
Ricky Ford <rickyf@fergusoncc.com>
2025-04-28 13:11:29 -0600

    Setup folders and files

---
+++ torn-spy-viewer/torn-spy-viewer.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name			Torn Spy Viewer
+// @namespace		noxwaste.com
+// @version			0.0.1
+// @description		Pull spy reports from your faction to display across Torn.
+// @author			Noxwaste [1811486]
+// @match			https://www.torn.com/*
+// @icon			https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @updateURL		
+// @downloadURL		
+// @run-at			document-end
+// @grant			GM.xmlHttpRequest
+// @grant			GM_setValue
+// @grant			GM_getValue
+// @grant			GM_info
+// @connect			api.torn.com
+// @connect			www.tornstats.com
+// @connect			yata.yt
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	// This is a test
+})();
